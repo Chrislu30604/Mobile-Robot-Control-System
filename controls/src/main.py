a935c14158f006b60d709ef2d5a9e08db093eacc
Chrislu <Chrislu30604@gmail.com>
2019-03-12 18:07:39 +0800

    PEP

--- controls/src/main.py
+++ controls/src/main.py
@@ -14,20 +14,23 @@ from listener import Listener
 
 HOST = ''
 PORT = {
-    'LASER':8001,
-    'ENC'  :8002,
-    'VEL'  :8003  
+    'LASER': 8001,
+    'ENC': 8002,
+    'VEL': 8003
 }
+
 
 def main():
     rospy.init_node("Scanner_Laser")
     rospy.loginfo("Start Running Node")
     rate = rospy.Rate(10)
-    
-    # Subscribe 
+
+    # Subscribe
     sub_laser = rospy.Subscriber('/scan', LaserScan, myrostools.callback_laser)
-    sub_Renc = rospy.Subscriber('/Rencoder', Int16, myrostools.callback_Rencoder)
-    sub_Lenc = rospy.Subscriber('/Lencoder', Int16, myrostools.callback_Lencoder)
+    sub_Renc = rospy.Subscriber(
+        '/Rencoder', Int16, myrostools.callback_Rencoder)
+    sub_Lenc = rospy.Subscriber(
+        '/Lencoder', Int16, myrostools.callback_Lencoder)
     # Publishes
     pub_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)
     walk_wheel = WalkWheel(pub_vel)
@@ -36,10 +39,11 @@ def main():
     laser_t = Listener(work_laser, HOST, PORT['LASER'])
     enc_t = Listener(work_enc, HOST, PORT['ENC'])
     vel_t = Listener(work_vel, HOST, PORT['VEL'], walk_wheel)
-    threadManger = [laser_t, enc_t, vel_t] # Wrapper
-    
-    map(lambda obj:(obj.setDaemon(True), obj.start()), threadManger)
+    threadManger = [laser_t, enc_t, vel_t]  # Wrapper
+
+    map(lambda obj: (obj.setDaemon(True), obj.start()), threadManger)
     walk_wheel.run()
+
 
 if __name__ == '__main__':
     main()
